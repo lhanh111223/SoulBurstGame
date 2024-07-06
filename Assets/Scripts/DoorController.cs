f4327aaac56b87e3ed5cfd9a7a8b1453f28b5ecd
HaLBH <halbhhe176636@fpt.edu.vn>
2024-07-06 23:31:12 +0700

    Update map

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DoorController : MonoBehaviour
+{
+    public Tilemap doorTilemap;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            doorTilemap.gameObject.SetActive(true);
+        }
+    } 
+}
